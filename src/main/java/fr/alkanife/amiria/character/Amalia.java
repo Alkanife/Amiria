252bec93d579f57281cab1d6d8420e0db89d9ce3
Alkanife <alkanife@gmail.com>
2020-07-09 21:51:04 +0200

    ✨ Added Amalia

---
+++ src/main/java/fr/alkanife/amiria/character/Amalia.java
@@ -0,0 +1,94 @@
+package fr.alkanife.amiria.character;
+
+public class Amalia extends Character {
+
+    @Override
+    public String lastName() {
+        return "Hyvern";
+    }
+
+    @Override
+    public String name() {
+        return "Amalia";
+    }
+
+    @Override
+    public String title() {
+        return "Esprit de la forêt";
+    }
+
+    @Override
+    public String race() {
+        return "Elementaire";
+    }
+
+    @Override
+    public String birth() {
+        return null;
+    }
+
+    @Override
+    public String age() {
+        return null;
+    }
+
+    @Override
+    public String sex() {
+        return "Feminin";
+    }
+
+    @Override
+    public int floor() {
+        return -1;
+    }
+
+    @Override
+    public String specialization() {
+        return "Mage Assassin";
+    }
+
+    @Override
+    public String weapon() {
+        return "Magie";
+    }
+
+    @Override
+    public String secondaryWeapon() {
+        return null;
+    }
+
+    @Override
+    public String characterTraits() {
+        return "Amalia est une véritable tête de mule, mais sait prendre des décisions lorsque c'est nécéssaire.";
+    }
+
+    @Override
+    public String aspect() {
+        return "Jeune femme aux cheveux d'une teinte verte, au regard farouche et noisette. Elle semble également de petite taille";
+    }
+
+    @Override
+    public String ownPower() {
+        return "Contrôle des plantes";
+    }
+
+    @Override
+    public String state() {
+        return null;
+    }
+
+    @Override
+    public String owner() {
+        return "Sheele";
+    }
+
+    @Override
+    public String ownerInspiration() {
+        return "Amalia (Wakfu), Arbre Mojo (The Legend of Zelda: Ocarina of Time)";
+    }
+
+    @Override
+    public String image() {
+        return "amalia.jpg";
+    }
+}
--- src/main/java/fr/alkanife/amiria/character/CharacterManager.java
+++ src/main/java/fr/alkanife/amiria/character/CharacterManager.java
@@ -14,6 +14,7 @@ public class CharacterManager {
         characters.add(new Akuma());
         characters.add(new Alexia());
         characters.add(new Aliedad());
+        characters.add(new Amalia());
     }
     
     public Character getCharacter(String name) {
