c14e9e649bc91666739d24098524ada91fbfb20f
Alkanife <39438814+Alkanife@users.noreply.github.com>
2020-07-20 20:02:57 +0200

    ✨ Added a welcome message system & some modifications

--- src/main/java/fr/alkanife/amiria/Amiria.java
+++ src/main/java/fr/alkanife/amiria/Amiria.java
@@ -5,14 +5,22 @@ import fr.alkanife.amiria.command.CommandHandler;
 import fr.alkanife.amiria.command.Commands;
 import fr.alkanife.amiria.event.EventListener;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Amiria {
 
     private static CommandHandler commandHandler = new CommandHandler();
     private static CharacterManager characterManager = new CharacterManager();
+
+    private static String enyxia = "341997146519633922";
+    private static String hrp = "468442309311070252";
 
     public static void main(String[] args) {
         try {
@@ -30,7 +38,11 @@ public class Amiria {
 
             commandHandler.registerCommands(new Commands());
 
-            JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+            JDABuilder jdaBuilder = JDABuilder.createDefault(token, Arrays.asList(GatewayIntent.GUILD_MEMBERS,
+                    GatewayIntent.GUILD_BANS,
+                    GatewayIntent.GUILD_EMOJIS,
+                    GatewayIntent.GUILD_PRESENCES,
+                    GatewayIntent.GUILD_MESSAGES));
             jdaBuilder.addEventListeners(new EventListener());
             jdaBuilder.build();
         } catch (Exception exception) {
@@ -44,5 +56,13 @@ public class Amiria {
 
     public static CharacterManager getCharacterManager() {
         return characterManager;
+    }
+
+    public static String getEnyxia() {
+        return enyxia;
+    }
+
+    public static String getHrp() {
+        return hrp;
     }
 }
--- src/main/java/fr/alkanife/amiria/event/EventListener.java
+++ src/main/java/fr/alkanife/amiria/event/EventListener.java
@@ -2,35 +2,73 @@ package fr.alkanife.amiria.event;
 
 import fr.alkanife.amiria.Amiria;
 import fr.alkanife.amiria.Log;
-import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class EventListener extends ListenerAdapter {
+import javax.annotation.Nonnull;
+import java.util.Random;
 
-    @Override
-    public void onReady(ReadyEvent event) {
-
-        if (event.getGuildTotalCount() > 1) {
-            Log.log("Je suis faite pour aider un seul et unique discord.");
-            event.getJDA().shutdown();
-            return;
-        }
-
-        Guild home = event.getJDA().getGuildById("341997146519633922");
-
-        if (home == null) {
-            Log.log("Je ne trouve pas ma maison...");
-            event.getJDA().shutdown();
-            return;
-        }
-
-        Log.log("Connectée !");
-    }
+public class EventListener implements net.dv8tion.jda.api.hooks.EventListener {
 
     @Override
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        Amiria.getCommandHandler().handle(event.getMessage());
+    public void onEvent(@Nonnull GenericEvent genericEvent) {
+        if (genericEvent instanceof ReadyEvent) {
+            ReadyEvent event = (ReadyEvent) genericEvent;
+
+            if (event.getGuildTotalCount() > 1) {
+                Log.log("Je suis faite pour aider un seul et unique discord.");
+                event.getJDA().shutdown();
+                return;
+            }
+
+            if (event.getJDA().getGuildById(Amiria.getEnyxia()) == null) {
+                Log.log("Je ne trouve pas ma maison...");
+                event.getJDA().shutdown();
+                return;
+            }
+
+            if (event.getJDA().getTextChannelById(Amiria.getHrp()) == null) {
+                Log.log("Euh... Je trouve pas le salon #hrp !?");
+                event.getJDA().shutdown();
+                return;
+            }
+
+            Log.log("Connectée !");
+
+        } else if (genericEvent instanceof GuildMessageReceivedEvent) {
+            Amiria.getCommandHandler().handle(((GuildMessageReceivedEvent) genericEvent).getMessage());
+
+        } else if (genericEvent instanceof GuildMemberJoinEvent) {
+            GuildMemberJoinEvent event = (GuildMemberJoinEvent) genericEvent;
+
+            if (!event.getGuild().getId().equals(Amiria.getEnyxia()))
+                return;
+
+            String[] welcomeMessages = {
+                    "Bienvenue mention ! Mets toi bien.",
+                    "Incroyable ! mention nous a rejoint.",
+                    "Bienvenue dans la réalité mention, prends une chaise.",
+                    "Nous vous acceptons dans la confrérie, mention.",
+                    "mention joined the game.",
+                    "Salut mention, dit-moi, comment va ta mère ?",
+                    "<:OMG:367670846438768640> UN NOUVEL ARRIVANT !! ACCUEILLEZ mention COMME IL SE DOIT !",
+                    "<Désolé mention, ce message a dû être supprimé pour droits d'auteur.>"
+            };
+
+            int random = new Random().nextInt(welcomeMessages.length);
+
+            String message = welcomeMessages[random];
+
+            TextChannel hrp = event.getGuild().getTextChannelById(Amiria.getHrp());
+
+            if (hrp != null)
+                hrp.sendMessage(message.replaceAll("mention", event.getMember().getAsMention())).queue();
+
+            event.getGuild().modifyMemberRoles(event.getMember(), event.getJDA().getRoleById("342983397217402880")).queue(aVoid
+                    -> Log.log("Ajout du rôle par défaut à " + event.getMember().getUser().getName()));
+        }
     }
 }
